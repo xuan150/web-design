c71cece8a0d1b89cf36e404dc9fb10d199911d31
xuan150 <waxd369@gmail.com>
2025-04-19 00:42:19 +0800

    home update

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,37 @@
 'use client'
 import Image from 'next/image'
 // import AvatarImg from "@/../public/avatar.jpg";
-import HeartImg from '@/../public/heart.png'
+// import HeartImg from '@/../public/heart.png'
 import { useState, useEffect, useRef } from 'react'
 import TrueFocus from "../component/TrueFocus"
+import Booble from "@/../public/bobo.svg"
+import Explosion from "@/../public/explosion.svg"
 
 export default function Home() {
-  const [heart, setHeart] = useState(0)
+  const [stage, setStage] = useState(0)
 
-  const addHeart = function () {
-    setHeart(heart + 1)
-  }
+  const maxStage = 8
 
-  const handleAnimationComplete = () => {
-    console.log('Animation completed!');
+  const handleClick = () => {
+    if (stage < maxStage) {
+      setStage(stage + 1);
+    } else {
+      setStage("explode"); // 進入爆炸狀態
+      setTimeout(() => setStage(0), 1000); // 爆炸動畫後重置
+    }
   };
+
+  let bubbleClass =
+    "transition-all duration-300 flex items-center justify-center cursor-pointer";
+    if (stage === 1) bubbleClass += " scale-105";
+    if (stage === 2) bubbleClass += " scale-110";
+    if (stage === 3) bubbleClass += " scale-115";
+    if (stage === 4) bubbleClass += " scale-120";
+    if (stage === 5) bubbleClass += " scale-130";
+    if (stage === 6) bubbleClass += " scale-140";
+    if (stage === 7) bubbleClass += " scale-150";
+    if (stage === 8) bubbleClass += " scale-160";
+    if (stage === "explode") bubbleClass += " opacity-0 scale-200";
 
   // // 背景圈圈
   // const blobRef = useRef(null)
@@ -39,34 +56,31 @@ export default function Home() {
 
   return (
     // 方法一:預設隱藏(invisible)，大於一定尺寸後(sm:invisible)右邊的頁面出現
-    <div className="relative overflow-hidden w-full h-full flex flex-col bg-[#DFF3FF]  justify-center items-center rounded-2xl">
+    <div className="relative overflow-hidden w-full h-full flex flex-col bg-[#DFF3FF]  justify-center items-center rounded-2xl text-[#64748B] gap-20">
       {/* <div
         className="absolute top-0 left-0 w-[100px] h-[100px] rounded-full bg-[#ffffff] blur-[50px] transition-all duration-450 
         ease-out pointer-events-none -translate-0.5"
         ref={blobRef}
       >
         {/* 幫妳加一個酷酷的東東(顏色可以自己調整) */}
-        {/* 用法：聯絡 chaotingchen10@gmail.com aka 妳的嚕嚕寶 */}
+      {/* 用法：聯絡 chaotingchen10@gmail.com aka 妳的嚕嚕寶 */}
       {/* </div> */}
 
       <TrueFocus
-        sentence="boo boo boo"
+        sentence="讓  它  爆  炸  吧"
         manualMode={true}
-        blurAmount={5}
-        borderColor="yellow"
+        blurAmount={20}
+        borderColor="rgba(255, 212, 100, 1)"
         animationDuration={0.5}
-        className="text-2xl mb-4"
       />
 
 
       <div
-        className="cursor-pointer flex flex-col justify-center items-center"
-        onClick={addHeart}
-      >
+        className="cursor-pointer flex flex-col justify-center items-center">
+          <div className={bubbleClass} onClick={handleClick}>{stage === "explode" ? (<Image src={Explosion} className="w-[70px] mb-2.5" alt="bubble" />):(<Image src={Booble} className="w-[70px] mb-2.5" alt="bubble" />)}</div>
+          <div className="mt-4 text-2xl text-[#64748B]">{stage === "explode" ? "Boom!" : stage}</div>
         {/* <Image width={200} height={200} src="/heart.png"></Image> */}
-        <Image src={HeartImg} className="w-[70px]" alt="heart" />
-        {/* <div>愛心</div> */}
-        <div className="text-2xl">{heart}</div>
+        {/* <div className="text-2xl">{heart}</div> */}
       </div>
     </div>
   )
